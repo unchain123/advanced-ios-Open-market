edd9cc670048d74005899694bfa402ff82f1915d
unchain123 <unchain123@kakao.com>
2022-11-16 16:42:39 +0900

    feat: Storyboard삭제 및 Window설정

---
+++ ChainMarket/ChainMarket/Sources/Entries/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  ChainMarket
+//
+//  Created by 오경식 on 2022/11/16.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+
+        let marketItemListViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: marketItemListViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}

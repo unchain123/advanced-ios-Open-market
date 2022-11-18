05458b95021d5ff1f364dc8af0f9ab677a74855b
unchain123 <unchain123@kakao.com>
2022-11-18 17:17:20 +0900

    feat: MarketItemListViewModel 구현

--- ChainMarket/ChainMarket/MarketItemListView/Viewmodel/MarketListViewModel.swift
+++ ChainMarket/ChainMarket/MarketItemListView/Viewmodel/MarketListViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+final class MarketItemListViewModel {
+
+    private let useCase: MarketItemListUseCase
+    private var listener: ((MarketItem) -> Void)?
+    private(set) var marketItems: [MarketItem] = []
+
+    init(useCase: MarketItemListUseCase = MarketItemListUseCase()) {
+        self.useCase = useCase
+    }
+
+    func bind(_ listener: @escaping (MarketItem) -> Void) {
+        self.listener = listener
+    }
+
+    func list() {
+        useCase.fetchedItem { [weak self] result in
+            switch result {
+            case .success(let Items):
+                self?.marketItems.append(contentsOf: Items)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
